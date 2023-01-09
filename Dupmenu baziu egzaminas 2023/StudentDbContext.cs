6425392872689a0047d602da88f64aa4aa4c0457
DariusB79 <d.balsevicius@gmail.com>
2023-01-09 06:55:29 +0200

    Add project files.

---
+++ Dupmenu baziu egzaminas 2023/StudentDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dupmenu_baziu_egzaminas_2023
+{
+    public class StudentDbContext : DbContext
+    {
+            public DbSet<Student> Students { get; set; }
+
+            public DbSet<Departament> Departaments { get; set; }
+
+            public DbSet<Lesson> Lessons { get; set; }
+
+
+            protected override void OnConfiguring(DbContextOptionsBuilder options)
+            {
+                options.UseSqlServer("Server=DESKTOP-17930GK\\TEW_SQLEXPRESS;Initial Catalog=DB_EGZ;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            }
+
+        }
+}
